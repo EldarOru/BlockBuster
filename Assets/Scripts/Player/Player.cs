3bd6edf4dd72f2be914761274caaa82dccc6dc3c
EldarOru <macdog108@gmail.com>
2020-11-01 00:52:41 +0300

    Initial commit

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int health = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void collidedWithEnemy(Enemy enemy)
+    {
+        enemy.Attack(this);
+        if (health <= 0)
+        {
+            // Todo 
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+
+        Enemy enemy = col.collider.gameObject.GetComponent<Enemy>();
+        if (enemy)
+        {
+            collidedWithEnemy(enemy);
+        }
+    }
+
+}
